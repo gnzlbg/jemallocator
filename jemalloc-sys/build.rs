15eb477f99f250a31fbf70126c5719f09dac5db6
Simon Sapin <simon.sapin@exyr.org>
2018-06-25 09:23:31 +0200

    Document in code why symbol prefixes are always enabled on some platforms.

--- jemalloc-sys/build.rs
+++ jemalloc-sys/build.rs
@@ -176,6 +176,13 @@ fn main() {
     let mut use_prefix =
         env::var_os("CARGO_FEATURE_UNPREFIXED_MALLOC_ON_SUPPORTED_PLATFORMS").is_none();
 
+    // “it was found that the `realpath` function in libc would allocate with libc malloc
+    //  (not jemalloc malloc), and then the standard library would free with jemalloc free,
+    //  causing a segfault.”
+    // https://github.com/rust-lang/rust/commit/e3b414d8612314e74e2b0ebde1ed5c6997d28e8d
+    // https://github.com/rust-lang/rust/commit/536011d929ecbd1170baf34e09580e567c971f95
+    // https://github.com/rust-lang/rust/commit/9f3de647326fbe50e0e283b9018ab7c41abccde3
+    // https://github.com/rust-lang/rust/commit/ed015456a114ae907a36af80c06f81ea93182a24
     if !use_prefix &&
         (target.contains("android")
             || target.contains("dragonfly")
