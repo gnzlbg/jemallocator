d94d3e58639e0f29e0bdbd3b1a5f994978caafb6
gnzlbg <gonzalobg88@gmail.com>
2018-10-24 13:50:17 +0200

    make run.sh POSIX compliant

--- ci/run.sh
+++ ci/run.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-: ${TARGET?"The TARGET environment variable must be set."}
+: "${TARGET?The TARGET environment variable must be set.}"
 
 echo "Running tests for target: ${TARGET}, Rust version=${TRAVIS_RUST_VERSION}"
 export RUST_BACKTRACE=1
@@ -14,28 +14,32 @@ export CARGO_CMD=cross
 #export JEMALLOC_SYS_RUN_TESTS=1
 
 # Use cargo on native CI platforms:
-if [[ ${TARGET} = *"windows"* ]] || \
-       [[ ${TARGET} = *"x86_64-unknown-linux-gnu"* ]] || \
-       [[ ${TARGET} = *"i686-unknown-linux-gnu"* ]] || \
-       [[ ${TARGET} = *"i586-unknown-linux-gnu"* ]] \
-       || [[ ${TARGET} = *"apple"* ]]; then
-    export CARGO_CMD=cargo
-else
+case "${TARGET}" in
+    "i586-unknown-linux-gnu") export CARGO_CMD=cargo ;;
+    "i686-unknown-linux-gnu") export CARGO_CMD=cargo ;;
+    "x86_64-unknown-linux-gnu") export CARGO_CMD=cargo ;;
+    *"windows"*) export CARGO_CMD=cargo ;;
+    *"apple"*) export CARGO_CMD=cargo ;;
+esac
+
+if [ "${CARGO_CMD}" = "cross" ]
+then
     cargo install cross || echo "cross is already installed"
 fi
 
-${CARGO_CMD} test -vv --target $TARGET
-${CARGO_CMD} test -vv --target $TARGET --features profiling
-${CARGO_CMD} test -vv --target $TARGET --features debug
-${CARGO_CMD} test -vv --target $TARGET --features stats
-${CARGO_CMD} test -vv --target $TARGET --features 'debug profiling'
-${CARGO_CMD} test -vv --target $TARGET --features unprefixed_malloc_on_supported_platforms
-${CARGO_CMD} test -vv --target $TARGET --release
-${CARGO_CMD} test -vv --target $TARGET -p jemalloc-sys
-${CARGO_CMD} test -vv --target $TARGET -p jemalloc-sys --features unprefixed_malloc_on_supported_platforms
-${CARGO_CMD} test -vv --target $TARGET -p systest
+${CARGO_CMD} test -vv --target "${TARGET}"
+${CARGO_CMD} test -vv --target "${TARGET}" --features profiling
+${CARGO_CMD} test -vv --target "${TARGET}" --features debug
+${CARGO_CMD} test -vv --target "${TARGET}" --features stats
+${CARGO_CMD} test -vv --target "${TARGET}" --features 'debug profiling'
+${CARGO_CMD} test -vv --target "${TARGET}" --features unprefixed_malloc_on_supported_platforms
+${CARGO_CMD} test -vv --target "${TARGET}" --release
+${CARGO_CMD} test -vv --target "${TARGET}" -p jemalloc-sys
+${CARGO_CMD} test -vv --target "${TARGET}" -p jemalloc-sys --features unprefixed_malloc_on_supported_platforms
+${CARGO_CMD} test -vv --target "${TARGET}" -p systest
 
-if [[ ${TRAVIS_RUST_VERSION} == "nightly"  ]]; then
+if [ "${TRAVIS_RUST_VERSION}" = "nightly"  ]
+then
     # The Alloc trait is unstable:
-    ${CARGO_CMD} test -vv --target $TARGET --features alloc_trait
+    ${CARGO_CMD} test -vv --target "${TARGET}" --features alloc_trait
 fi
