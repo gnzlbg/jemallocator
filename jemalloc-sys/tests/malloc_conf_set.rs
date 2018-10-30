d9463c3f725fa4ede1847cb4fb80fae19dc6c7f9
gnzlbg <gonzalobg88@gmail.com>
2018-10-30 20:34:36 +0100

    reformat

--- jemalloc-sys/tests/malloc_conf_set.rs
+++ jemalloc-sys/tests/malloc_conf_set.rs
@@ -3,14 +3,18 @@ extern crate libc;
 
 union U {
     x: &'static u8,
-    y: &'static libc::c_char
+    y: &'static libc::c_char,
 }
 
 #[allow(non_upper_case_globals)]
 #[cfg_attr(prefixed, export_name = "_rjem_malloc_conf")]
 #[cfg_attr(not(prefixed), no_mangle)]
-pub static malloc_conf: Option<&'static libc::c_char>
-    = Some(unsafe { U { x: &b"abort:true\0"[0] }.y });
+pub static malloc_conf: Option<&'static libc::c_char> = Some(unsafe {
+    U {
+        x: &b"abort:true\0"[0],
+    }
+    .y
+});
 
 #[test]
 fn malloc_conf_set() {
