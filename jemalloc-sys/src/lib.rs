ae6448b60e769c31054ed971d3e1a038a1bd3fcd
gnzlbg <gonzalobg88@gmail.com>
2018-11-03 17:01:27 +0100

    use clippy on CI

--- jemalloc-sys/src/lib.rs
+++ jemalloc-sys/src/lib.rs
@@ -41,6 +41,10 @@
 //! [jemalloc_mallctl]: http://jemalloc.net/jemalloc.3.html#mallctl_namespace
 #![no_std]
 #![allow(non_snake_case, non_camel_case_types)]
+#![cfg_attr(
+    feature = "cargo-clippy",
+    allow(clippy::cast_possible_truncation, clippy::cast_possible_wrap)
+)]
 
 extern crate libc;
 
